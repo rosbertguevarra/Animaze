fdb2fe0e9b5c5858f91e7f021f6dc48d05a8cd66
rosbertguevarra <crisiscorefair@gmail.com>
2019-09-10 09:26:33 +0800

    modfied version-2

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./Navbar";
 import Carousel from "./Carousel";
 import Upcoming from "./Upcoming";
+import Featured from "./Featured";
 
 export default class Main extends Component {
   render() {
--- src/components/Upcoming.js
+++ src/components/Upcoming.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import jikan from "../api/jikan";
 import "../style/style.css";
+import Featured from "./Featured";
 
 export default class Upcoming extends Component {
   state = {
@@ -9,6 +10,7 @@ export default class Upcoming extends Component {
   componentDidMount = async () => {
     const response = await jikan.get("/top/anime/1/upcoming");
     this.setState({ shows: response.data.top });
+    console.log("mounted");
   };
 
   render() {
@@ -24,40 +26,6 @@ export default class Upcoming extends Component {
                   <img src={animes.image_url} />
                   <span>{animes.title}</span>
                 </div>
-
-                // <div className="card" style={{ width: "18rem" }}>
-                //   <span>{animes.rank}</span>
-                //   <img
-                //     src={animes.image_url}
-                //     className="card-img-top"
-                //     alt="..."
-                //   />
-                //   <div className="card-body">
-                //     <h5 className="card-title">{animes.title}</h5>
-                //     <span>
-                //       Type: <span className="description">{animes.type}</span>
-                //     </span>
-                //     <span>
-                //       Episodes:{" "}
-                //       <span className="description">{animes.episodes}</span>
-                //     </span>
-                //     <span>
-                //       Start date:{" "}
-                //       <span className="description">{animes.start_date}</span>{" "}
-                //     </span>
-                //     <span>
-                //       End date:{" "}
-                //       <span className="description">{animes.end_date}</span>
-                //     </span>
-                //     <span>
-                //       Members:{" "}
-                //       <span className="description">{animes.members}</span>
-                //     </span>
-                //     <a href="#" className="btn btn-primary">
-                //       Go somewhere
-                //     </a>
-                //   </div>
-                // </div>
               );
             })}
           </div>
